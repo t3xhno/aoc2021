313898d259431f8a687e3697897a26f4d4e91589
t3x <t3xhnolainx@gmail.com>
2021-12-02 11:58:29 +0100

    Day02 | More functional

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,41 +1,43 @@
 use std::error;
+use itertools::Itertools;
+
 
 use aoc2021::{input::Input, config::Config};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let config = Config::new()?;
     let file_contents = Input::new(&config.day).as_string()?;
-    let data: Vec<(&str, i32)> = file_contents.lines().map(input_mapper).collect();
+    let data = parse_input(&file_contents);
+
     println!("{}", solve1(&data));
     println!("{}", solve2(&data));
+
     Ok(())
 }
 
-fn input_mapper(line: &str) -> (&str, i32) {
-    let mut tuple: (&str, i32) = ("", 0);
-    let split = line.trim().split(" ").collect::<Vec<&str>>();
-    tuple.0 = split[0]; tuple.1 = split[1].parse::<i32>().unwrap();
-    tuple
+fn parse_input(input: &str) -> Vec<(u8, i32)> {
+    input.split_whitespace()
+        .tuples()
+        .map(|(op, i)| (op.as_bytes()[0], i.parse::<i32>().unwrap()))
+        .collect::<Vec<(u8, i32)>>()
 }
 
-fn solve1(data: &Vec<(&str, i32)>) -> i32 {
-    let mut depth = 0; let mut dist = 0;
-    data.into_iter().for_each(|op| match op {
-        ("up", x) => depth -= x,
-        ("down", x) => depth += x,
-        (_, x) => dist += x,
-    });
-    depth * dist
+fn solve1(data: &Vec<(u8, i32)>) -> i32 {
+    let (hor, ver) = data.iter()
+        .fold((0, 0), |(hor, ver), (op, i)| match op {
+            b'f' => (hor + i, ver),
+            b'd' => (hor, ver + i),
+            _ => (hor, ver - i),
+        });
+    hor * ver
 }
 
-fn solve2(data: &Vec<(&str, i32)>) -> i32 {
-    let mut depth = 0; let mut dist = 0; let mut aim = 0;
-    data.into_iter().for_each(|op| match op {
-        ("down", x) => aim += x,
-        ("up", x) => aim -= x,
-        (_, x) => {
-            dist += x; depth += x * aim;
-        }
-    });
-    depth * dist
+fn solve2(data: &Vec<(u8, i32)>) -> i32 {
+    let (hor, ver, _) = data.iter()
+        .fold((0, 0, 0), |(hor, ver, aim), (op, i)| match op {
+            b'f' => (hor + i, ver + aim * i, aim),
+            b'd' => (hor, ver, aim + i),
+            _ => (hor, ver, aim - i),
+        });
+    hor * ver
 }
