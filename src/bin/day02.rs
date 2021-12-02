e17820afb7415aa0b71e51ef5e28caa594a71a1c
t3x <t3xhnolainx@gmail.com>
2021-12-02 18:49:55 +0100

    Day02 | Refactored the runner

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -7,8 +7,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let config = Config::new()?;
     let file_contents = Input::new(&config.day).as_string()?;
     let data = parse_input(&file_contents);
-    println!("{}", solve1(&data, &mut Submarine::new()));
-    println!("{}", solve2(&data, &mut Submarine::new()));
+    println!("{}", solve(&data, &mut Submarine::new(), config.part));
     Ok(())
 }
 
@@ -19,12 +18,10 @@ fn parse_input(input: &str) -> Vec<(u8, i32)> {
         .collect()
 }
 
-fn solve1(data: &[(u8, i32)], sub: &mut Submarine) -> i32 {
-    data.iter().for_each(|(op, i)| sub.move_part_1(*op, *i));
-    sub.dist()
-}
-
-fn solve2(data: &[(u8, i32)], sub: &mut Submarine) -> i32 {
-    data.iter().for_each(|(op, i)| sub.move_part_2(*op, *i));
+fn solve(data: &[(u8, i32)], sub: &mut Submarine, part: u8) -> i32 {
+    data.iter().for_each(|(op, i)| match part {
+        1 => sub.move_part_1(*op, *i),
+        _ => sub.move_part_2(*op, *i),
+    });
     sub.dist()
 }
